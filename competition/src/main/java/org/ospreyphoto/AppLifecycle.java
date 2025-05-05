920f88b883eba3267909130bf70c4e7fa49470e7
MBWhite 2021 <nospam@mh-white.com>
2025-05-05 16:55:29 +0100

    app tray
    Signed-off-by: MBWhite 2021 <nospam@mh-white.com>

---
+++ competition/src/main/java/org/ospreyphoto/AppLifecycle.java
@@ -0,0 +1,200 @@
+package org.ospreyphoto;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.awt.AWTException;
+import java.awt.CheckboxMenuItem;
+import java.awt.Desktop;
+import java.awt.Image;
+import java.awt.Menu;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+
+@ApplicationScoped
+public class AppLifecycle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppLifecycle.class);
+
+    void onStart(@Observes StartupEvent ev) {
+        LOGGER.info("The application is starting...");
+
+        /* Use an appropriate Look and Feel */
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+
+        //Schedule a job for the event-dispatching thread:
+        //adding TrayIcon.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {
+        LOGGER.info("The application is stopping...");
+    }
+
+    private void createAndShowGUI() {
+        //Check the SystemTray support
+        if (!SystemTray.isSupported()) {
+            System.out.println("SystemTray is not supported");
+            return;
+        }
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource("osprey-icon.gif");
+        LOGGER.info(resource.toString());
+        final PopupMenu popup = new PopupMenu();
+
+        final TrayIcon trayIcon = new TrayIcon(new ImageIcon(resource, "tray icon").getImage());
+        trayIcon.setImageAutoSize(true);
+        trayIcon.setToolTip("Osprey");
+        final SystemTray tray = SystemTray.getSystemTray();
+
+        // Create a popup menu components
+        MenuItem aboutItem = new MenuItem("About");
+
+        Menu displayMenu = new Menu("Competition");
+        MenuItem errorItem = new MenuItem("Summary");
+        MenuItem warningItem = new MenuItem("Display");
+
+        MenuItem exitItem = new MenuItem("Exit");
+
+        //Add components to popup menu
+        popup.add(aboutItem);
+        // popup.addSeparator();
+        // popup.add(cb1);
+        // popup.add(cb2);
+        popup.addSeparator();
+        popup.add(displayMenu);
+        displayMenu.add(errorItem);
+        displayMenu.add(warningItem);
+        popup.add(exitItem);
+
+        trayIcon.setPopupMenu(popup);
+
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("TrayIcon could not be added.");
+            return;
+        }
+
+        trayIcon.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,
+                        "This dialog box is run from System Tray");
+            }
+        });
+
+        aboutItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,
+                        "This dialog box is run from the About menu item");
+            }
+        });
+
+        // cb1.addItemListener(new ItemListener() {
+        //     public void itemStateChanged(ItemEvent e) {
+        //         int cb1Id = e.getStateChange();
+        //         if (cb1Id == ItemEvent.SELECTED) {
+        //             trayIcon.setImageAutoSize(true);
+        //         } else {
+        //             trayIcon.setImageAutoSize(false);
+        //         }
+        //     }
+        // });
+
+        // cb2.addItemListener(new ItemListener() {
+        //     public void itemStateChanged(ItemEvent e) {
+        //         int cb2Id = e.getStateChange();
+        //         if (cb2Id == ItemEvent.SELECTED) {
+        //             trayIcon.setToolTip("Sun TrayIcon");
+        //         } else {
+        //             trayIcon.setToolTip(null);
+        //         }
+        //     }
+        // });
+
+        ActionListener listener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                MenuItem item = (MenuItem) e.getSource();
+                //TrayIcon.MessageType type = null;
+                System.out.println(item.getLabel());
+                if ("Error".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.ERROR;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is an error message", TrayIcon.MessageType.ERROR);
+
+                } else if ("Warning".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.WARNING;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is a warning message", TrayIcon.MessageType.WARNING);
+
+                } else if ("Info".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.INFO;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is an info message", TrayIcon.MessageType.INFO);
+
+                } else if ("None".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.NONE;
+                    if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                        try {
+                            Desktop.getDesktop().browse(new URI("http://www.example.com"));
+                        } catch (IOException | URISyntaxException e1) {
+                            // TODO Auto-generated catch block
+                            e1.printStackTrace();
+                        }
+                    }
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is an ordinary message", TrayIcon.MessageType.NONE);
+                }
+            }
+        };
+
+        errorItem.addActionListener(listener);
+        warningItem.addActionListener(listener);
+
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tray.remove(trayIcon);
+                System.exit(0);
+            }
+        });
+    }
+
+}
